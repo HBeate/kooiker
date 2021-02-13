07dabac51c1262a5fc8b4f034dfb6cd5db2d3253
HBeate <hauser_b@hotmail.com>
2021-02-13 12:46:25 +0100

    aboutus 0.1

--- src/helper/Constants.js
+++ src/helper/Constants.js
@@ -2,7 +2,9 @@ const baseURL = "https://api.kooiker-fr.com";
 
 module.exports = {
   breeding: baseURL + "/kooiker/items/breeding?fields=*.*.*",
-  aboutus: baseURL + "/kooiker/items/aboutus?fields=*.*"
+  aboutus: baseURL + "/kooiker/items/aboutus?fields=*.*.*",
+  aboutus_trans : baseURL + "/kooiker/items/aboutus_translations?filter[language][eq]=",
+  aboutus_gallery: baseURL + "/kooiker/items/aboutus_directus_files?fields=*.*",
 };
 /* 
 services: baseURL + "/ivanahairart/items/services?fields=*.*&sort=nr_to_sort",  // Link verändert
@@ -10,4 +12,5 @@ contact: baseURL + "/ivanahairart/items/contact?fields=*.*",
 ivana : baseURL + "/ivanahairart/items/ivana?fields=*.*",
 legal : baseURL + "/ivanahairart/items/legal?fields=*.*",
 jobs : baseURL + "/ivanahairart/items/jobs?fields=*.*", */
+
 
