d6fec10118b9b8bca79b9f5bab1614e1fb3aca22
der62sim79 <ali.tonc@gmx.at>
2021-02-20 11:11:22 +0100

    reveal

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,80 +1,90 @@
 import React, { Component } from "react";
 import styles from "./Home.module.css";
+import Fade from "react-reveal/Fade";
+import Zoom from 'react-reveal/Zoom';
 
 class Home extends Component {
   state = {};
 
-  
   render() {
     return (
       <div className={styles.container}>
         <div className={styles.parallax}></div>
         <div className={styles.parallax}>
+          <Zoom>
           <div className={styles.header}>
             <h1>Kooikerhondje “de la bande de rigolos“</h1>
           </div>
+          </Zoom>
         </div>
         <div className={styles.parallax}></div>
         <div className={styles.parallax}>
-        <div className={styles.content}>
-          <p>
-            Lorem ipsum dolor sit amet, harum paulo moderatius sea no, duo
-            vocent mediocrem ne. Eum vidisse torquatos voluptatum ad, sea et
-            legere doctus dissentiunt. Mei quas essent invidunt ea, cum civibus
-            antiopam in. Et ullum dolores quo, commodo lobortis volutpat at mea.
-            Ius albucius argumentum ne, cum ut consetetur quaerendum, te aeque
-            tollit est. Autem ipsum est ut, euismod recusabo id quo. Vel cu
-            liber invenire, adhuc graeco nonumes ei quo, cu eos alia dicant. Cum
-            decore scriptorem eu, ne movet dolore pro, erant graeco has ut. Zril
-            liberavisse intellegebat sit ex, at cetero option consequuntur vix.
-            At reque appetere quaestio vel. Usu nulla dicunt ei. No illum
-            feugait nostrum has. Vim ipsum clita conclusionemque ei. Ad mundi
-            euripidis interesset nec, vim ad invidunt sententiae. In cum eripuit
-            molestiae, habemus noluisse pro ad. Pri prima meliore adipiscing cu,
-            veniam necessitatibus has eu. Graece iriure mei id, nec tota porro
-            aliquip cu. Ad copiosae luptatum vim, cu atqui phaedrum omittantur
-            sit, id erat mundi sed. Cu vel harum dicant molestie. Sit ne viris
-            feugiat, porro salutandi assueverit ut sea, vide pertinax eu nam. Id
-            cum tritani epicuri. Mucius apeirian cum id. Te vel illud partiendo
-            splendide, ad simul consul facilisis nam. Eum ex malis meliore
-            admodum, ius graecis deserunt facilisis in. Eos ad utinam labores
-            invidunt, decore feugiat est at. Ferri iusto lobortis pro ad, an
-            partem legendos per. An ius tempor delenit efficiantur, ius ad augue
-            debet detracto. No sed harum ubique. Vel probo quodsi volumus ad.
-            Numquam gloriatur vituperata qui te, no altera petentium vim.
-          </p>
-        </div>
+          <Fade left>
+            <div className={styles.content}>
+              <p>
+                Lorem ipsum dolor sit amet, harum paulo moderatius sea no, duo
+                vocent mediocrem ne. Eum vidisse torquatos voluptatum ad, sea et
+                legere doctus dissentiunt. Mei quas essent invidunt ea, cum
+                civibus antiopam in. Et ullum dolores quo, commodo lobortis
+                volutpat at mea. Ius albucius argumentum ne, cum ut consetetur
+                quaerendum, te aeque tollit est. Autem ipsum est ut, euismod
+                recusabo id quo. Vel cu liber invenire, adhuc graeco nonumes ei
+                quo, cu eos alia dicant. Cum decore scriptorem eu, ne movet
+                dolore pro, erant graeco has ut. Zril liberavisse intellegebat
+                sit ex, at cetero option consequuntur vix. At reque appetere
+                quaestio vel. Usu nulla dicunt ei. No illum feugait nostrum has.
+                Vim ipsum clita conclusionemque ei. Ad mundi euripidis
+                interesset nec, vim ad invidunt sententiae. In cum eripuit
+                molestiae, habemus noluisse pro ad. Pri prima meliore adipiscing
+                cu, veniam necessitatibus has eu. Graece iriure mei id, nec tota
+                porro aliquip cu. Ad copiosae luptatum vim, cu atqui phaedrum
+                omittantur sit, id erat mundi sed. Cu vel harum dicant molestie.
+                Sit ne viris feugiat, porro salutandi assueverit ut sea, vide
+                pertinax eu nam. Id cum tritani epicuri. Mucius apeirian cum id.
+                Te vel illud partiendo splendide, ad simul consul facilisis nam.
+                Eum ex malis meliore admodum, ius graecis deserunt facilisis in.
+                Eos ad utinam labores invidunt, decore feugiat est at. Ferri
+                iusto lobortis pro ad, an partem legendos per. An ius tempor
+                delenit efficiantur, ius ad augue debet detracto. No sed harum
+                ubique. Vel probo quodsi volumus ad. Numquam gloriatur
+                vituperata qui te, no altera petentium vim.
+              </p>
+            </div>
+          </Fade>
         </div>
         <div className={styles.parallax}></div>
         <div className={styles.parallax}>
-        <div className={styles.content}>
-          <p>
-            Lorem ipsum dolor sit amet, harum paulo moderatius sea no, duo
-            vocent mediocrem ne. Eum vidisse torquatos voluptatum ad, sea et
-            legere doctus dissentiunt. Mei quas essent invidunt ea, cum civibus
-            antiopam in. Et ullum dolores quo, commodo lobortis volutpat at mea.
-            Ius albucius argumentum ne, cum ut consetetur quaerendum, te aeque
-            tollit est. Autem ipsum est ut, euismod recusabo id quo. Vel cu
-            liber invenire, adhuc graeco nonumes ei quo, cu eos alia dicant. Cum
-            decore scriptorem eu, ne movet dolore pro, erant graeco has ut. Zril
-            liberavisse intellegebat sit ex, at cetero option consequuntur vix.
-            At reque appetere quaestio vel. Usu nulla dicunt ei. No illum
-            feugait nostrum has. Vim ipsum clita conclusionemque ei. Ad mundi
-            euripidis interesset nec, vim ad invidunt sententiae. In cum eripuit
-            molestiae, habemus noluisse pro ad. Pri prima meliore adipiscing cu,
-            veniam necessitatibus has eu. Graece iriure mei id, nec tota porro
-            aliquip cu. Ad copiosae luptatum vim, cu atqui phaedrum omittantur
-            sit, id erat mundi sed. Cu vel harum dicant molestie. Sit ne viris
-            feugiat, porro salutandi assueverit ut sea, vide pertinax eu nam. Id
-            cum tritani epicuri. Mucius apeirian cum id. Te vel illud partiendo
-            splendide, ad simul consul facilisis nam. Eum ex malis meliore
-            admodum, ius graecis deserunt facilisis in. Eos ad utinam labores
-            invidunt, decore feugiat est at. Ferri iusto lobortis pro ad, an
-            partem legendos per. An ius tempor delenit efficiantur, ius ad augue
-            debet detracto. No sed harum ubique. Vel probo quodsi volumus ad.
-            Numquam gloriatur vituperata qui te, no altera petentium vim.
-          </p>
-        </div>
+        <Fade right>
+          <div className={styles.content}>
+            <p>
+              Lorem ipsum dolor sit amet, harum paulo moderatius sea no, duo
+              vocent mediocrem ne. Eum vidisse torquatos voluptatum ad, sea et
+              legere doctus dissentiunt. Mei quas essent invidunt ea, cum
+              civibus antiopam in. Et ullum dolores quo, commodo lobortis
+              volutpat at mea. Ius albucius argumentum ne, cum ut consetetur
+              quaerendum, te aeque tollit est. Autem ipsum est ut, euismod
+              recusabo id quo. Vel cu liber invenire, adhuc graeco nonumes ei
+              quo, cu eos alia dicant. Cum decore scriptorem eu, ne movet dolore
+              pro, erant graeco has ut. Zril liberavisse intellegebat sit ex, at
+              cetero option consequuntur vix. At reque appetere quaestio vel.
+              Usu nulla dicunt ei. No illum feugait nostrum has. Vim ipsum clita
+              conclusionemque ei. Ad mundi euripidis interesset nec, vim ad
+              invidunt sententiae. In cum eripuit molestiae, habemus noluisse
+              pro ad. Pri prima meliore adipiscing cu, veniam necessitatibus has
+              eu. Graece iriure mei id, nec tota porro aliquip cu. Ad copiosae
+              luptatum vim, cu atqui phaedrum omittantur sit, id erat mundi sed.
+              Cu vel harum dicant molestie. Sit ne viris feugiat, porro
+              salutandi assueverit ut sea, vide pertinax eu nam. Id cum tritani
+              epicuri. Mucius apeirian cum id. Te vel illud partiendo splendide,
+              ad simul consul facilisis nam. Eum ex malis meliore admodum, ius
+              graecis deserunt facilisis in. Eos ad utinam labores invidunt,
+              decore feugiat est at. Ferri iusto lobortis pro ad, an partem
+              legendos per. An ius tempor delenit efficiantur, ius ad augue
+              debet detracto. No sed harum ubique. Vel probo quodsi volumus ad.
+              Numquam gloriatur vituperata qui te, no altera petentium vim.
+            </p>
+          </div>
+          </Fade>
         </div>
         <div className={styles.parallax}></div>
       </div>
