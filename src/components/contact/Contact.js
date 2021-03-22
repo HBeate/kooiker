77e3c88cca90825903b48e9a7d2ae22d1e6760f7
der62sim79 <ali.tonc@gmx.at>
2021-03-22 19:02:27 +0100

    contact map and email link first comit

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Form from '../Form/Form';
+import styles from './Contact.module.css';
 
 class Contact extends Component {
     state = {}
@@ -7,11 +8,29 @@ class Contact extends Component {
         return (
             <div>  
                 <div>
-                    {/*                 <a href="tel:+1-555-555-1212">555-555-1212</a>
- */}                <a href="tel:+15555551212">555-555-1212</a>
+                <iframe 
+                title="GoogleMap" 
+                src="https://www.google.com/maps/embed?pb=!1m17!1m8!1m3!1d11540.194209343405!2d0.0047!3d43.688754!3m2!1i1024!2i768!4f13.1!4m6!3e6!4m0!4m3!3m2!1d43.6902871!2d0.004663!5e0!3m2!1sde!2sus!4v1616435576475!5m2!1sde!2sus" 
+                width="100%" 
+                height="450" 
+                style={{ border: 0 }} 
+                allowfullscreen="" 
+                loading="lazy">
+                </iframe>
                 </div>
-                <div>
-                    <Form/>
+                <div className={styles.headerKontakt}>
+                <h1>Kontakt</h1>
+                </div>
+                
+                <div className={styles.contact2}>
+                    <div className={styles.mailContact}>
+                        <p>Chemin de Lanegrand; 32400 Fustérouau</p>
+                        <a className={styles.iFrame} href = "mailto: helga-henny@kooiker-fr.com">
+                        <i  class="far fa-envelope"> </i>
+                        helga-henny@kooiker-fr.com
+                        </a>
+                    </div>
+                     <Form/>
                 </div>
 
             </div>
